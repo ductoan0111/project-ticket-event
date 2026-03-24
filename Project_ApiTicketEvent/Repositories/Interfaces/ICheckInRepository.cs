491bd95935fee99df9d8cb5416e1059581ee2f13
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-03-24 10:02:22 +0700

    update

---
+++ Project_ApiTicketEvent/Repositories/Interfaces/ICheckInRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models.DTOs.Reponses;
+using Models.DTOs.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interfaces
+{
+    public interface ICheckInRepository
+    {
+        object Checkin(CheckInRequest req);
+    }
+}
