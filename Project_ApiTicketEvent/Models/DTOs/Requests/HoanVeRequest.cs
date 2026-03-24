ffa587c27d1db97d53f4da40e52047fcc404a99b
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-03-24 21:05:12 +0700

    update

---
+++ Project_ApiTicketEvent/Models/DTOs/Requests/HoanVeRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Requests
+{
+    public class HoanVeRequest
+    {
+        public string? LyDo { get; set; }
+        public string? PhuongThuc { get; set; } = "REFUND_MOCK";
+        public string? RawResponse { get; set; }
+    }
+}
