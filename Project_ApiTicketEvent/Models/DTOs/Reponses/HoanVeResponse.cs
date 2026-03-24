688eb75923c825b6a33a0441eb1d1447247bf38c
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-03-24 21:04:47 +0700

    update

---
+++ Project_ApiTicketEvent/Models/DTOs/Reponses/HoanVeResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Reponses
+{
+    public class HoanVeResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = "";
+
+        public int? VeID { get; set; }
+        public int? DonHangID { get; set; }
+
+        // Số tiền hoàn (mình trả về dạng dương cho dễ hiểu)
+        public decimal RefundAmount { get; set; }
+
+        public int? ThanhToanID { get; set; }
+        public string? MaGiaoDich { get; set; }
+    }
+}
