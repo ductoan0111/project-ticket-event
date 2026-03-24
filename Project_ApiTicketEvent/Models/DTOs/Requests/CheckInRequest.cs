492e44ae48c1c37f29ca7fbc4eafc50a2cab0078
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-03-24 10:01:56 +0700

    fix

---
+++ Project_ApiTicketEvent/Models/DTOs/Requests/CheckInRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Requests
+{
+    public class CheckInRequest
+    {
+        public string? QrToken { get; set; }
+        public string? MaVe { get; set; } // optional (nếu muốn check-in bằng mã vé)
+        public int NhanVienID { get; set; }
+        public string? GhiChu { get; set; }
+    }
+}
